844d05cc85d86635f14d620a9576cdd52268f316
Alessandro Mello <alessandromellojunior@gmail.com>
2026-09-03 12:07:11 -0300

    ++

--- src/app/area-do-cliente/page.js
+++ src/app/area-do-cliente/page.js
@@ -1,7 +1,7 @@
-import HeroSlide from "@/pageComponents/home/HeroSlide";
 import NovidadesSection from "@/pageComponents/home/NovidadesSection";
 import BannerIndique from "@/pageComponents/indique-e-ganhe-leste/BannerIndique";
 import AppHighlight from "@/pageComponents/area-cliente/AppHighlight";
+import AreaClienteHeroBanner from "@/pageComponents/area-cliente/AreaClienteHeroBanner";
 import CanaisAtendimentoClienteSection from "@/pageComponents/area-cliente/CanaisAtendimentoClienteSection";
 import TireDuvidasSection from "@/pageComponents/area-cliente/TireDuvidasSection";
 import WelcomeHelpSection from "@/pageComponents/area-cliente/WelcomeHelpSection";
@@ -9,7 +9,7 @@ import WelcomeHelpSection from "@/pageComponents/area-cliente/WelcomeHelpSection
 export default function AreaDoClientePage() {
   return (
     <main className="min-h-full bg-light">
-      <HeroSlide />
+      <AreaClienteHeroBanner />
       <WelcomeHelpSection />
       <BannerIndique />
       <CanaisAtendimentoClienteSection />
